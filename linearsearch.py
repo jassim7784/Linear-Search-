bda8e7f984e168491af8782c7a135fe463cefdd9
Muhammed Jassim Saji <95706343+jassim7784@users.noreply.github.com>
2024-08-05 23:42:38 +0530

    Create linearsearch.py

---
+++ linearsearch.py
@@ -0,0 +1,12 @@
+def linear_search(arr, target):
+  for i in range(len(arr)):
+    if arr[i] == target
+     return i
+  return -1 
+array = list(map(int, input("enter the array"))
+target = int(input())
+result = linear_search(arr, target)
+if result != -1:
+  print("Element found at index")
+else:
+  print("element not found")
